46050cc240558bc0bdc67b4e90f9ecf783931619
Plesu Oana <plesu.oana@gmail.com>
2017-05-28 10:33:17 +0300

    changed ide from inteliij to eclipse

--- src/HolidayPlanner/DBConnection.java
+++ src/HolidayPlanner/DBConnection.java
@@ -1,3 +1,5 @@
+package HolidayPlanner;
+
 import java.sql.*;
 import java.util.ArrayList;
 
--- src/HolidayPlanner/User.java
+++ src/HolidayPlanner/User.java
@@ -1,3 +1,5 @@
+package HolidayPlanner;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
